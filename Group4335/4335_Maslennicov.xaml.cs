0ad65023060e82d0201d3952ecbb5aecf63ff121
IBASIK2000 <ibasshalopaev@gmail.com>
2026-03-19 14:45:01 +0300

    Create 4335_Maslennicov.xaml.cs

---
+++ Group4335/4335_Maslennicov.xaml.cs
@@ -0,0 +1,17 @@
+using System.Windows;
+
+namespace Template4335
+{
+    public partial class _4335_Masleennicov : Window
+    {
+        public _4335_Maslennicov()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
